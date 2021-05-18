838ed1bc7ad8b1c19464dc8e1147bd77665da2e4
Kartik Agrawal <kartik.agrawal1_cs18@gla.ac.in>
2021-05-18 20:26:25 +0530

    changes

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
-import Item from './Item';
 import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CheckIcon from '@material-ui/icons/Check';
 const ToDolist = () => {
 
-    const [item, setItem] = useState("");
+    const [done] = useState({decorate:false, opaque:false});
+    const [item, setItem] = useState({name:"", textDecoration: "", opacity:""});
     const [updatedItem, setUpdated] = useState([]);
     const update = (e) => {
-        setItem(e.target.value);
+        setItem({...item,name:e.target.value});
     }
     const show = () => {
         if (item!=="") {
-            setUpdated((old)=>{
-                return [...old, item];
-            });
-            setItem("");
+            const res = {name:item.name,textDecoration:done.decorate,opacity:done.opaque}
+            setUpdated([...updatedItem,res]);
+            setItem({name:"",textDecoration:"",opacity:""});
+            console.log(updatedItem);
         }
         else{
             alert("Please enter some Task")
@@ -26,18 +28,44 @@ const ToDolist = () => {
                 return index !== id;
             });
         });
+        console.log(updatedItem);
     };
+
+    const done1 = (id) => {
+        const newArr = updatedItem.map((val,index)=>{
+            if (index===id) {
+                if (val.textDecoration === false && val.opacity === false) {
+                    val.textDecoration = true;
+                    val.opacity = true;   
+                }
+                else{
+                    val.textDecoration = false;
+                    val.opacity = false;
+                }
+            }
+            return val;
+        })
+        setUpdated(newArr);
+    }
     return(
         <div className="main">
             <p className="heading">TODO LIST</p>
             <div className="main1">
-                <input type="text" placeholder="Add an item" value={item} onChange={update} />
+                <input type="text" placeholder="Add an item" value={item.name} onChange={update} />
                 <button onClick={show}><AddIcon /></button>
             </div>
             {
                 updatedItem.map((val,index)=>{
                     return(
-                        <Item val={val} key={index} id={index} onSelect={del} />
+                        <div className="item" key={index}>
+                            <DeleteIcon className="delete" fontSize="large" onClick={()=>{
+                                del(index);
+                            }} />
+                            <CheckIcon className="edit" fontSize="large" onClick={()=>{
+                                done1(index);
+                            }} />
+                            <p className="mainItem" style={{textDecoration:val.textDecoration?"line-through":"none",opacity:val.opacity?"0.7":"1"}}>{val.name}</p>
+                        </div>
                     );
                 })
             }
